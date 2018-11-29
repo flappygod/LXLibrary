737bc781a985a02796c9048c17525a7b45fd6572
lijunlin <327603258@qq.com>
2018-11-29 21:06:18 +0800

    修改

--- lxlibrary/src/main/java/com/flappygod/lipo/lxlibrary/Tools/CoordinateTool.java
+++ lxlibrary/src/main/java/com/flappygod/lipo/lxlibrary/Tools/CoordinateTool.java
@@ -94,4 +94,35 @@ public class CoordinateTool {
     }
 
 
+
+    /********
+     * 计算两个经纬度之间的距离
+     *
+     * @param lat1
+     *            第一点纬度
+     * @param lng1
+     *            第一点经度
+     * @param lat2
+     *            第二点纬度
+     * @param lng2
+     *            第二点经度
+     * @return
+     */
+    public static double distanceByLngLat(double lat1, double lng1,
+                                          double lat2, double lng2) {
+        double radLat1 = lat1 * Math.PI / 180;
+        double radLat2 = lat2 * Math.PI / 180;
+        double a = radLat1 - radLat2;
+        double b = lng1 * Math.PI / 180 - lng2 * Math.PI / 180;
+        double s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2)
+                + Math.cos(radLat1) * Math.cos(radLat2)
+                * Math.pow(Math.sin(b / 2), 2)));
+        /****
+         *  取WGS84标准参考椭球中的地球长半径(单位:m)
+         */
+        s = s * 6378137.0;
+        s = Math.round(s * 10000) / 10000;
+        return s;
+    }
+
 }
