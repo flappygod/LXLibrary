43c6a284c0fca1cc840f266b736ed4cff7fcbc2d
lijunlin <327603258@qq.com>
2018-12-08 11:44:45 +0800

    修改bug

--- lxlibrary/src/main/java/com/flappygod/lipo/lxlibrary/Tools/MapJumpTool.java
+++ lxlibrary/src/main/java/com/flappygod/lipo/lxlibrary/Tools/MapJumpTool.java
@@ -2,6 +2,7 @@ package com.flappygod.lipo.lxlibrary.Tools;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.text.TextUtils;
 
 import java.io.File;
@@ -16,12 +17,12 @@ public class MapJumpTool {
     /**
      * 启动高德App进行导航
      *
-     * @param appname 必填 第三方调用应用名称。如 amap
-     * @param destination       非必填 POI 名称
-     * @param lat               必填 纬度
-     * @param lon               必填 经度
-     * @param dev               必填 是否偏移(0:lat 和 lon 是已经加密后的,不需要国测加密; 1:需要国测加密)
-     * @param style             必填 导航方式(0 速度快; 1 费用少; 2 路程短; 3 不走高速；4 躲避拥堵；5 不走高速且避免收费；6 不走高速且躲避拥堵；7 躲避收费和拥堵；8 不走高速躲避收费和拥堵))
+     * @param appname     必填 第三方调用应用名称。如 amap
+     * @param destination 非必填 POI 名称
+     * @param lat         必填 纬度
+     * @param lon         必填 经度
+     * @param dev         必填 是否偏移(0:lat 和 lon 是已经加密后的,不需要国测加密; 1:需要国测加密)
+     * @param style       必填 导航方式(0 速度快; 1 费用少; 2 路程短; 3 不走高速；4 躲避拥堵；5 不走高速且避免收费；6 不走高速且躲避拥堵；7 躲避收费和拥堵；8 不走高速躲避收费和拥堵))
      */
     public static void goToNaviAmapActivity(Context context,
                                             String appname,
@@ -44,6 +45,43 @@ public class MapJumpTool {
                         .append("&style=").append(style);
 
                 Intent intent = new Intent("android.intent.action.VIEW", android.net.Uri.parse(stringBuffer.toString()));
+                intent.setPackage("com.autonavi.minimap");
+                context.startActivity(intent);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    /**
+     * 启动高德App进行路径规划
+     *
+     * @param context  上下文
+     * @param sAddress 起点地址
+     * @param slat     起点地址
+     * @param slon     起点地址
+     * @param dlat     终点地址
+     * @param dlon     终点地址
+     * @param dAddress 终点地址
+     * @param dev      必填 是否偏移(0:lat 和 lon 是已经加密后的,不需要国测加密; 1:需要国测加密)
+     * @param style    必填 导航方式(0 速度快; 1 费用少; 2 路程短; 3 不走高速；4 躲避拥堵；5 不走高速且避免收费；6 不走高速且躲避拥堵；7 躲避收费和拥堵；8 不走高速躲避收费和拥堵))
+     */
+    public static void goToWayAmapActivity(Context context,
+                                           String slat,
+                                           String slon,
+                                           String sAddress,
+                                           String dlat,
+                                           String dlon,
+                                           String dAddress,
+                                           String dev,
+                                           String style) {
+
+        if (isInstallByRead("com.autonavi.minimap")) {
+            try {
+                Uri uri = Uri.parse("amapuri://route/plan/?sid=BGVIS1&slat=" + slat + "&slon=" + slon + "&sname=" + sAddress +
+                        "&did=BGVIS2&dlat=" + dlat + "&dlon=" + dlon + "&dname=" + dAddress + "&dev=" + dev + "&t=" + style);
+                Intent intent = new Intent("android.intent.action.VIEW", uri);
                 intent.setPackage("com.autonavi.minimap");
                 context.startActivity(intent);
             } catch (Exception e) {
@@ -84,6 +122,40 @@ public class MapJumpTool {
                 e.printStackTrace();
             }
         }
+    }
+
+
+    /**********
+     * 前往百度地图的路径规划
+     * @param context    上下文
+     * @param slat       起点地址
+     * @param slon       起点地址
+     * @param sAddress   起点地址
+     * @param dlat       终点地址
+     * @param dlon       终点地址
+     * @param dAddress   终点地址
+     */
+    public static void goToWayBaiduActivity(Context context,
+                                            String slat,
+                                            String slon,
+                                            String sAddress,
+                                            String dlat,
+                                            String dlon,
+                                            String dAddress) {
+
+        if (isInstallByRead("com.baidu.BaiduMap")) {
+            try {
+                String uri = String.format("baidumap://map/direction?origin=%s,%s&destination=" +
+                                "%s,%s&mode=driving&src=com.flappygod.lipo", slat, slon,
+                        dlat, dAddress);
+                Intent intent = new Intent();
+                intent.setData(Uri.parse(uri));
+                context.startActivity(intent);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 
 
